2381fe6f7a276c71d6aa20d14d4bb53636014bd2
Yuriysps <166153092+Yuriysps@users.noreply.github.com>
2025-03-10 15:33:39 +0200

    Add files via upload

---
+++ js/task-5.js
@@ -0,0 +1,18 @@
+console.log('task-5.js');
+const btnChange = document.querySelector(".change-color");
+const bodyColor = document.querySelector("body");
+const spanText = document.querySelector(".color");
+
+btnChange.addEventListener("click", (event) => {
+  const newColor = getRandomHexColor();
+  bodyColor.style.backgroundColor = newColor;
+  spanText.textContent = newColor;
+  
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
---
+++ js/task-6.js
@@ -0,0 +1,44 @@
+console.log('task-6.js');
+
+const inputNumber = document.querySelector("input");
+const btnCreate = document.querySelector("[data-create]");
+const btnDestroy = document.querySelector("[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+
+btnCreate.addEventListener("click", (event) => {
+ const amount = parseInt(inputNumber.value, 10);
+  if (amount > 0 && amount <= 100) {
+    createBoxes(amount);
+    inputNumber.value = ''; 
+  }
+  
+})
+
+
+ btnDestroy.addEventListener("click", destroyBoxes);
+
+
+function createBoxes(amount) {
+  const markup = [];
+  
+
+  for (let i = 0; i < amount; i++)
+  {
+    const size = 30 + i * 10;
+    const color = getRandomHexColor();
+   markup.push(`<div class="new-box" style="width: ${size}px; height: ${size}px; background-color: ${color};"></div>`); 
+    
+  }
+         boxesContainer.innerHTML = markup.join('');
+}
+
+function destroyBoxes() {
+   boxesContainer.innerHTML = '';
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
